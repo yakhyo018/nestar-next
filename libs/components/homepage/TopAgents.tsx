c217b872b9d6e1ce82658127e1e11b481d755b96
Antony(Yahyo) <choriyevyahyo@gmail.com>
2026-09-21 18:08:08 +0900

    fix: modify TopProperties and TopAgents sectional  components

--- libs/components/homepage/TopAgents.tsx
+++ libs/components/homepage/TopAgents.tsx
@@ -8,6 +8,9 @@ import { Autoplay, Navigation, Pagination } from 'swiper';
 import TopAgentCard from './TopAgentCard';
 import { Member } from '../../types/member/member';
 import { AgentsInquiry } from '../../types/member/member.input';
+import { useQuery } from '@apollo/client';
+import { GET_AGENTS } from '../../../apollo/user/query';
+import { T } from '../../types/common';
 
 interface TopAgentsProps {
 	initialInput: AgentsInquiry;
@@ -20,6 +23,20 @@ const TopAgents = (props: TopAgentsProps) => {
 	const [topAgents, setTopAgents] = useState<Member[]>([]);
 
 	/** APOLLO REQUESTS **/
+	const {
+		loading: getAgentsLoading,
+		data: getAgentsData,
+		error: getAgentsError,
+		refetch: getAgentsRefetch,
+	} = useQuery(GET_AGENTS, {
+		fetchPolicy: 'cache-and-network',
+		variables: { input: initialInput },
+		notifyOnNetworkStatusChange: true,
+		onCompleted: (data: T) => {
+			setTopAgents(data?.getAgents?.list ?? []);
+		},
+	});
+
 	/** HANDLERS **/
 
 	if (device === 'mobile') {
@@ -103,7 +120,7 @@ TopAgents.defaultProps = {
 	initialInput: {
 		page: 1,
 		limit: 10,
-		sort: 'memberRank',
+		sort: 'memberRanks',
 		direction: 'DESC',
 		search: {},
 	},
