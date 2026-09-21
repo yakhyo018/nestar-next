49b6977a5f09b376755534954343f648dba4ff3f
Antony(Yahyo) <choriyevyahyo@gmail.com>
2026-09-21 14:39:56 +0900

    feat: develop TrendProperties component based on react query  data

--- libs/components/homepage/TrendProperties.tsx
+++ libs/components/homepage/TrendProperties.tsx
@@ -8,6 +8,9 @@ import { Autoplay, Navigation, Pagination } from 'swiper';
 import { Property } from '../../types/property/property';
 import { PropertiesInquiry } from '../../types/property/property.input';
 import TrendPropertyCard from './TrendPropertyCard';
+import { GET_PROPERTIES } from '../../../apollo/user/query';
+import { useQuery } from '@apollo/client';
+import { T } from '../../types/common';
 
 interface TrendPropertiesProps {
 	initialInput: PropertiesInquiry;
@@ -19,6 +22,19 @@ const TrendProperties = (props: TrendPropertiesProps) => {
 	const [trendProperties, setTrendProperties] = useState<Property[]>([]);
 
 	/** APOLLO REQUESTS **/
+	const {
+		loading: getPropertiesLoading,
+		data: getPropertiesData,
+		error: getPropertiesError,
+		refetch: getPropertiesRefetch,
+	} = useQuery(GET_PROPERTIES, {
+		fetchPolicy: 'cache-and-network',
+		variables: { input: initialInput },
+		notifyOnNetworkStatusChange: true,
+		onCompleted: (data: T) => {
+			setTrendProperties(data?.getProperties?.list);
+		},
+	});
 	/** HANDLERS **/
 
 	if (trendProperties) console.log('trendProperties:', trendProperties);
